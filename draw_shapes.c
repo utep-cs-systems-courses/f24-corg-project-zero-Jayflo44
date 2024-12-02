d641375ae5a93643ae22b5663f9c76c28b2ecc1e
Jayflo44 <123122247+Jayflo44@users.noreply.github.com>
2024-12-01 18:09:36 -0700

    Final

--- draw_shapes.c
+++ draw_shapes.c
@@ -13,7 +13,6 @@ void print_square(int leftCol, int size)
     putchar('\n');
   }
 }
-
 // Prints a triangle of specified height whose left edge is at col leftCol.
 void print_triangle(int leftCol, int size)
 {
@@ -25,4 +24,20 @@ void print_triangle(int leftCol, int size)
     putchar('\n');
   }
 }
-
+void print_arrow(int leftCol, int size) {
+    // Print the upward arrow "head" (triangle)
+    for (int row = 0; row <= size; row++) {
+      int minCol = leftCol + size - row, maxCol = leftCol + size + row;
+        int col;
+        for (col = 0; col < minCol; col++) putchar(' ');
+	for(        ; col<= maxCol; col++) putchar('*');
+        putchar('\n');
+    }
+    int tailColStart = leftCol + size - 2;
+    int endCol = size / 1.5;
+    for (int row = 0; row < size; row++){
+      for (int col = 0; col < tailColStart; col++) putchar(' ');
+      for (int col = 0; col < endCol;  col++) putchar('*');
+      putchar('\n');
+    }
+}
--- uimain.c
+++ uimain.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "draw.h" // project-related declarations
-
 int main() 
 {
   puts("Welcome!");
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Arrow = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -17,17 +16,17 @@ int main()
     // Given the user answer, select which method to call
     switch (c) {
     case 't':
-      puts("You selected triangle:");
-      print_triangle(5, 7);
+      puts("You selected arrow:");
+      print_arrow(8, 8);
       break;
     case 's':
       puts("You selected square:");
-      print_square(5, 5);
+      print_square(8, 5);
       break;
     case 'c':
       puts("You selected chars:");
-      for (char c = 'a'; c < 'd'; c++)
-	print_char_5x7(c);
+      for (char c = 'A'; c < 'D'; c++)
+	print_char_11x16(c);
       break;
     case 'q':
       puts("Bye!");
